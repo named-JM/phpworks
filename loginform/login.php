56ae6bb7fba6c42577631812614d23c6d3e852f4
Echel Cags <joannacaguco@gmail.com>
2024-07-06 19:03:22 +0800

    add inputs and button styles

---
+++ loginform/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Inventory Management</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="header">
+        <h1>Login</h1>
+    </div>
+    <div class="container">
+        <form action="" method="POST">
+            <div>
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username" placeholder="Enter username"></div>
+            <br>
+            <div>
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" placeholder="Enter password"></form>
+            </div>
+            <div>
+                <button type="submit">Login</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
